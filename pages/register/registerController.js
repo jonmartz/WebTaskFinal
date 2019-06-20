ae9086bda1f516542664f471442100dac4f1855c
jonmartz <jonmartz91@gmail.com>
2019-06-20 20:39:22 +0300

    implemented register screen

--- pages/register/registerController.js
+++ pages/register/registerController.js
@@ -11,6 +11,9 @@ angular.module("myApp")
             if ($scope.userCategories.length < 2){
                 $scope.answer = "please select at least 2 categories";
             }
+            else if ($scope.question1 === $scope.question2){
+                $scope.answer = "please enter two DIFFERENT questions";
+            }
             else{
                 let failed = false;
                 $scope.answer = "";
@@ -21,55 +24,53 @@ angular.module("myApp")
                 let body = service.createBody("users",columnString, values);
                 $http.post('http://localhost:3000/register', body).then(
                     function successCallback(res) {
+                        document.getElementById("submitButton").disabled = true;
+                        document.getElementById("submitButton").style.visibility = "hidden";
+                        document.getElementById("submitText").style.color = "green";
+                        $scope.answer = "Registering...";
+
+                        // question 1
+                        columnString = "username+question+answer";
+                        values = [$scope.username, $scope.question1, $scope.answer1];
+                        body = service.createBody("question_and_answer",columnString, values);
+                        $http.post('http://localhost:3000/insert', body).then(
+                            function successCallback(res) {
+                            }
+                            , function errorCallback(res) {
+                                // $scope.answer = "error registering question 1: " + res.data;
+                            }
+                        );
+
+                        // question 2
+                        columnString = "username+question+answer";
+                        values = [$scope.username, $scope.question2, $scope.answer2];
+                        body = service.createBody("question_and_answer",columnString, values);
+                        $http.post('http://localhost:3000/insert', body).then(
+                            function successCallback(res) {
+                            }
+                            , function errorCallback(res) {
+                                // $scope.answer = "error registering question 2: " + res.data;
+                            }
+                        );
+                        let categoriesToAdd = $scope.userCategories;
+                        for (let i = 0; i < categoriesToAdd.length; i++){
+                            let category = categoriesToAdd[i];
+                            columnString = "Id+name";
+                            values = [$scope.username, category];
+                            body = service.createBody("category",columnString, values);
+                            $http.post('http://localhost:3000/insert', body).then(
+                                function successCallback(res) {
+                                }
+                                , function errorCallback(res) {
+                                    // $scope.answer = "error registering category" + (i+1) + ": " + res.data;
+                                }
+                            );
+                        }
                     }
                     , function errorCallback(res) {
                         $scope.answer = "Username already exists!"
-                        failed = true;
                     }
                 );
-                if (failed) return;
-                document.getElementById("submitButton").disabled = true;
-                document.getElementById("submitButton").style.visibility = "hidden";
-                document.getElementById("submitText").style.color = "green";
-                $scope.answer = "Registering...";
-
-                // question 1
-                columnString = "username+question+answer";
-                values = [$scope.username, $scope.question1, $scope.answer1];
-                body = service.createBody("question_and_answer",columnString, values);
-                $http.post('http://localhost:3000/insert', body).then(
-                    function successCallback(res) {
-                    }
-                    , function errorCallback(res) {
-                        // $scope.answer = "error registering question 1: " + res.data;
-                    }
-                );
-
-                // question 2
-                columnString = "username+question+answer";
-                values = [$scope.username, $scope.question2, $scope.answer2];
-                body = service.createBody("question_and_answer",columnString, values);
-                $http.post('http://localhost:3000/insert', body).then(
-                    function successCallback(res) {
-                    }
-                    , function errorCallback(res) {
-                        // $scope.answer = "error registering question 2: " + res.data;
-                    }
-                );
-                let categoriesToAdd = $scope.userCategories;
-                for (let i = 0; i < categoriesToAdd.length; i++){
-                    let category = categoriesToAdd[i];
-                    columnString = "Id+name";
-                    values = [$scope.username, category];
-                    body = service.createBody("category",columnString, values);
-                    $http.post('http://localhost:3000/insert', body).then(
-                        function successCallback(res) {
-                        }
-                        , function errorCallback(res) {
-                            // $scope.answer = "error registering category" + (i+1) + ": " + res.data;
-                        }
-                    );
-                }
             }
         };
     });
