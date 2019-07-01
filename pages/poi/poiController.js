eaaaa3a04bcfff9050be2686fe6f33745634ce90
Shahar Ezra <ezrasha@post.bgu.ac.il>
2019-07-01 21:53:01 +0300

    fixed bugs

--- pages/poi/poiController.js
+++ pages/poi/poiController.js
@@ -1,17 +1,37 @@
 // poi controller
 angular.module("myApp")
-.controller("poiController", function ($scope) {
+.controller("poiController", function ($scope, $http) {
     self = this;
     self.cities = {
         1: {name:"Paris", state: "France", image: "https://media-cdn.tripadvisor.com/media/photo-s/0d/f5/7c/f2/eiffel-tower-priority.jpg"},
         2: {name:"Jerusalem", state: "Israel", image: "https://cdni.rt.com/files/2017.12/article/5a3fe04efc7e93cd698b4567.jpg"},
         3: {name:"London", state: "England", image: "http://www.ukguide.co.il/Photos/England/London/British-Royal-Tour.jpg"}
     }
+    $scope.allCategories={};
+    $scope.relevantCategories={};
+    $http.get('http://localhost:3000/select/category/name/').then(function(response) {
+                $scope.allCategories = response.data;
+            }
+    )
+    $http.get('http://localhost:3000/select/pointOfinterest/name/').then(function(response) {
+                $scope.allCategories = response.data;
+            }
+    )
+    $scope.getRelevantCategories=function()
+    {
+        alert ($scope.allCategories);
+
+        for (let index = 4; index < $scope.allCategories.length; index++) {
+            delete $scope.allCategories[index];
+        }
+        alert ($scope.allCategories);
+    }
     $scope.noMatches = "";
     $scope.showFirst=true;
     self.currCities={};
     $scope.showAll=function()
     {
+        $scope.getRelevantCategories();
         $scope.noMatches = "";
         $scope.showFirst=true;
     }
