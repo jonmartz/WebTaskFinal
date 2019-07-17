83435c2af01eda411f1a478a0f303bf01f43760d
jonmartz <jonmartz91@gmail.com>
2019-07-17 15:41:50 +0300

    Implementing POI review

--- app.js
+++ app.js
@@ -95,3 +95,27 @@ app.service('service', function() {
         };
     }
 });
+
+app.service('modalService',['$q','$rootScope',function($q,$rootScope){
+
+    var s=this;    		// Self reference
+
+    // Attributes
+    s.modalOn=false;	// Flag to indicate if the modal is on or off. Close by default
+
+    s.openModal=function(){
+        s.defer=$q.defer();										// We create a deferrer
+        s.modalOn=true;												// Flag the showing of the modal
+        $rootScope.$broadcast('MODAL_OPEN');	// Broadcast the message that the popup is open
+        return s.defer.promise;								// Return a promise to the calling function
+    };
+
+    s.returnValue=function(value){
+
+        s.modalOn=false;												// We flag the closing of the modal
+        $rootScope.$broadcast('MODAL_CLOSE');		// Broadcast the event
+
+        s.defer.resolve(value);									// Return the resolved value of the modal
+    };
+
+}]);
--- pages/pointPage/pointPageController.js
+++ pages/pointPage/pointPageController.js
@@ -1,56 +1,102 @@
 angular.module("myApp")
-.controller("pointPageController", function ($scope, $http) {
-    $scope.name=window.location.hash.substring(13);
-    $scope.poiInfo={};
-    $scope.reviewInfo={};
-    $scope.relevantInfo={};
-    $http.get("http://localhost:3000/select/pointOfInterest/city,image,numOfViewers,description,rank/name="+'\''+$scope.name+'\'').then(function(response) {
-            $scope.poiInfo=response.data;
-        }
-    )
-    $http.get("http://localhost:3000/select/reviews/time,context/pointOfInterest="+'\''+$scope.name+'\'').then(function(response) {
-            $scope.reviewInfo=response.data;
-            if($scope.reviewInfo[0]!==undefined)
-            {
-                var currDates=$scope.relevantInfo;
-                var max1=new Date($scope.getFormattedDate($scope.reviewInfo[0].time));
-                var max2=new Date($scope.getFormattedDate($scope.reviewInfo[0].time));
-                var max1Idx=0, max2Idx=0;
-                for(let i=0;i<$scope.reviewInfo.length;i++)
+    .controller("pointPageController", function ($scope, $http, service, modalService) {
+        $scope.name=window.location.hash.substring(13);
+        $scope.poiInfo={};
+        $scope.reviewInfo={};
+        $scope.relevantInfo={};
+        $http.get("http://localhost:3000/select/pointOfInterest/city,image,numOfViewers,description,rank/name="+'\''+$scope.name+'\'').then(function(response) {
+                $scope.poiInfo=response.data;
+            }
+        );
+        $http.get("http://localhost:3000/select/reviews/time,context/pointOfInterest="+'\''+$scope.name+'\'').then(function(response) {
+                $scope.reviewInfo=response.data;
+                if($scope.reviewInfo[0]!==undefined)
                 {
-                    var a=new Date($scope.getFormattedDate($scope.reviewInfo[i].time));
-                    if(a.getTime() > max1)
+                    var currDates=$scope.relevantInfo;
+                    var max1=new Date($scope.getFormattedDate($scope.reviewInfo[0].time));
+                    var max2=new Date($scope.getFormattedDate($scope.reviewInfo[0].time));
+                    var max1Idx=0, max2Idx=0;
+                    for(let i=0;i<$scope.reviewInfo.length;i++)
                     {
-                        max1=a.getTime();
-                        max1Idx=i;
-                    }
-                }
-                currDates[0]=$scope.reviewInfo[max1Idx];
-                let c0=currDates[0].time;
-                currDates[0].time=(c0.replace(/T/," ")).substring(0, c0.length-1);
-                for(let i=0;i<$scope.reviewInfo.length;i++)
-                {
-                    var a = new Date($scope.getFormattedDate($scope.reviewInfo[i].time));
-                    if(a.getTime() !== max1)
-                    {
-                        if(a.getTime() > max2)
+                        var a=new Date($scope.getFormattedDate($scope.reviewInfo[i].time));
+                        if(a.getTime() > max1)
                         {
-                            max2=a.getTime();
-                            max2Idx=i;
+                            max1=a.getTime();
+                            max1Idx=i;
                         }
                     }
+                    currDates[0]=$scope.reviewInfo[max1Idx];
+                    let c0=currDates[0].time;
+                    currDates[0].time=(c0.replace(/T/," ")).substring(0, c0.length-1);
+                    for(let i=0;i<$scope.reviewInfo.length;i++)
+                    {
+                        var a = new Date($scope.getFormattedDate($scope.reviewInfo[i].time));
+                        if(a.getTime() !== max1)
+                        {
+                            if(a.getTime() > max2)
+                            {
+                                max2=a.getTime();
+                                max2Idx=i;
+                            }
+                        }
+                    }
+                    currDates[1]=$scope.reviewInfo[max2Idx];
+                    let c1=currDates[1].time;
+                    currDates[1].time=(c1.replace(/T/," ")).substring(0, c1.length-1);
+                    $scope.relevantInfo=currDates;
                 }
-                currDates[1]=$scope.reviewInfo[max2Idx];
-                let c1=currDates[1].time;
-                currDates[1].time=(c1.replace(/T/," ")).substring(0, c1.length-1);
-                $scope.relevantInfo=currDates;
             }
-        }
-    )
+        );
 
-    $scope.getFormattedDate=function(d)
-    {
-        var d3=d.replace(/Z/g,"");
-        return d3;
-    }
-});
+        if (service.username !== ""){
+            document.getElementById('notLoggedIn').style.visibility = "hidden";
+        }
+        else{
+            document.getElementById('reviewButton').disabled = true;
+            document.getElementById('saveButton').disabled = true;
+        }
+
+
+        $scope.getFormattedDate=function(d)
+        {
+            var d3=d.replace(/Z/g,"");
+            return d3;
+        };
+
+        $scope.writeReview=function() {
+            modalService.openModal().then(function(data){
+                window.alert(data);
+            });
+        };
+
+        $scope.saveToFavorites=function() {
+
+        };
+
+        var vm=this;
+        vm.show=modalService.modalOn;							// Flag to show or hide the modal
+        vm.returnValue=modalService.returnValue;	// Reference to the service function to resolve the promise
+
+        $scope.$on('MODAL_OPEN',function(){
+            vm.show=modalService.modalOn;
+        });
+
+        $scope.$on('MODAL_CLOSE',function(){
+            vm.show=modalService.modalOn;
+        });
+
+    });
+
+app.controller('modalController',['modalService','$scope',function(modalService, $scope){
+    var vm=this;
+    vm.show=modalService.modalOn;							// Flag to show or hide the modal
+    vm.returnValue=modalService.returnValue;	// Reference to the service function to resolve the promise
+
+    $scope.$on('MODAL_OPEN',function(){
+        vm.show=modalService.modalOn;
+    });
+
+    $scope.$on('MODAL_CLOSE',function(){
+        vm.show=modalService.modalOn;
+    });
+}]);
