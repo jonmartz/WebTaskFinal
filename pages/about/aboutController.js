049efc8c42d81c3eb63b8a5e97759f2922740747
Shahar Ezra <ezrasha@post.bgu.ac.il>
2019-07-20 21:07:47 +0300

    finished about page

--- pages/about/aboutController.js
+++ pages/about/aboutController.js
@@ -2,23 +2,6 @@
 angular.module("myApp")
     .controller("aboutController", function ($scope, $http, $window, service) {
         $scope.myFunc = function() {
-            if (service.username !== ""){
-                $scope.text = service.username + " is logged in"
-            }
-            else {
-                $scope.text = "no one logged in"
-            }
-            // $http({
-            //     method: 'GET',
-            //     url: "http://localhost:3000/private/select/countries/name",
-            //     headers: {
-            //         'x-auth-token': $window.sessionStorage.getItem("token")
-            //     }
-            // }).then(function successCallback(res) {
-            //         $scope.text = "valid token. country names: \n" + res.data;
-            //     }
-            //     , function errorCallback(res) {
-            //         $scope.text = "invalid token";
-            //     })
+
         }
     });
